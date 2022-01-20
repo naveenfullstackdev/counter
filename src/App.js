1ed21c62dc231cd7d311eeca7f0afd23de064493
Naveen Sharma <fullstackdevnaveen@gmail.com>
2022-01-20 19:42:00 +0530

    React Counter App Complete Project Folder Added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [currCount, updatedCount] = useState(0);
+
+  useEffect(() => {
+    alert("Hello, Counter will go from 1 to 25.");
+  }, []);
+
+  const incNum = () => {
+    if (currCount <= 24) updatedCount(currCount + 1);
+    else alert("Counter Limit Reached.");
+  };
+
+  const decNum = () => {
+    if (currCount !== 0) updatedCount(currCount - 1);
+    else alert("Counter cannot go Below 0.");
+  };
+
+  const backToZero = () => {
+    updatedCount(currCount - currCount);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{currCount}</h1>
+          <div className="btn_div">
+            <button onClick={incNum}>Increment</button>
+            <button onClick={decNum}>Decremment</button>
+          </div>
+          <button onClick={backToZero}>Reset</button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
